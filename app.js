e9e07ec0e2cfe5fbbd8c0e6bb7ff634f308f3736
pavelBaltean <pavel.baltean1999@gmail.com>
2020-03-05 22:40:57 +0200

    commit

--- app.js
+++ app.js
@@ -1,95 +1,75 @@
-let express = require('express');
-let app=express();
-let server=require('http').createServer(app);
-let bodyParser = require('body-parser');
-let dbWork=require('./dataBase.js');
-const mysql=require("mysql2");
+let express = require("express");
+let app = express();
+let server = require("http").createServer(app);
+let bodyParser = require("body-parser");
+let dbWork = require("./dataBase.js");
+const mysql = require("mysql2");
 //let path = require('path'); for wait html
 
 server.listen(3000);
 
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname + '/run.html');
-  
-  });
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/pages/run.html");
+});
 
-  
+app.post("/run", urlencodedParser, function(request, response) {
+  let url;
+  let latency;
+  let duration;
+  dbWork.truncateDataBase();
+  if (!request.body) return response.sendStatus(400);
 
-  app.post("/run", urlencodedParser, function (request, response) {
-    let url;
-    let latency;
-    let duration;
-    dbWork.truncateDataBase();
-    if(!request.body) return response.sendStatus(400);
+  url = request.body.url;
+  latency = request.body.latency;
+  duration = request.body.duration;
 
-    url=request.body.url;
-    latency=request.body.latency;
-    duration=request.body.duration;
+  response.sendFile(__dirname + "/pages/wait.html");
 
-    response.sendFile(__dirname + '/wait.html');
+  //it is for wait.html
+  //response.sendFile(path.join(__dirname, '/public', 'wait.html'), duration);
 
-    //it is for wait.html
-    //response.sendFile(path.join(__dirname, '/public', 'wait.html'), duration);
+  console.log("START");
+  console.log("");
+  console.log(request.body);
 
-    console.log("START");
-    console.log("");
-    console.log(request.body);
- 
-    let timerId = setInterval(() => dbWork.reqOnLinkSaveDB(url), latency*1000);
-  setTimeout(() => { clearInterval(timerId); console.log("STOP");}, duration*1000);
-  
-  });
+  let timerId = setInterval(() => dbWork.reqOnLinkSaveDB(url), latency * 1000);
+  setTimeout(() => {
+    clearInterval(timerId);
+    console.log("STOP");
+  }, duration * 1000);
+});
 
-  
-
-app.get('/getDataBase',function(req,res){
+app.get("/getDataBase", function(req, res) {
   const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     database: "mybase",
     password: "root"
   });
-  
-  connection.connect(function(err){
+
+  connection.connect(function(err) {
     if (err) {
       return console.error("Error: " + err.message);
-    }
-    else{
+    } else {
       console.log("Database is connected");
     }
   });
 
   console.log("getDataBase works");
-  connection.query('SELECT * FROM user', (err,rows) => {
-    
-    if(err) throw err;
-
-    //console.log(rows[0].responseTime);
-    // rows.forEach( (row) => {
-    //   let resTime=`${row.responseTime}`;
-    //   res.json(resTime);
-    //   console.log(resTime);
-    // });
-   
+  connection.query("SELECT * FROM user", (err, rows) => {
+    if (err) throw err;
     res.json(rows);
   });
-
 });
 
-app.get('/lineChart',function(req,res){
-  res.sendFile(__dirname + '/lineChart.html');
+app.get("/lineChart", function(req, res) {
+  res.sendFile(__dirname + "/pages/lineChart.html");
 });
 
-app.get('/pieChart',function(req,res){
-  res.sendFile(__dirname + '/pieChart.html');
+app.get("/pieChart", function(req, res) {
+  res.sendFile(__dirname + "/pages/pieChart.html");
 });
-
-
-
-
 
 //http://localhost:3000
-
-
--- dataBase.js
+++ dataBase.js
@@ -1,6 +1,5 @@
-const request = require('request');
-const mysql=require("mysql2");
-let express = require('express');
+const request = require("request");
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -9,13 +8,12 @@ const connection = mysql.createConnection({
   password: "root"
 });
 
-connection.connect(function(err){
+connection.connect(function(err) {
   if (err) {
     return console.error("Error: " + err.message);
-  }
-  else{
+  } else {
     let sql = "TRUNCATE TABLE user;";
-    connection.query(sql, function (err, result) {
+    connection.query(sql, function(err, result) {
       if (err) throw err;
       console.log("Table Truncated");
     });
@@ -23,76 +21,61 @@ connection.connect(function(err){
   }
 });
 
-function reqOnLinkSaveDB(reqURL){
-   request.get({   
-     url : reqURL,
-     time : true
-   },function(err, response){
+function reqOnLinkSaveDB(reqURL) {
+  request.get(
+    {
+      url: reqURL,
+      time: true
+    },
+    function(err, response) {
       let today = new Date();
       let resElapsedTime;
       let resStatusCode;
       let resStatusMessage;
-      
-      if(err){
-        resElapsedTime=0;
-        resStatusCode=404;
-        resStatusMessage="Not Found";
-       }else{
-        resElapsedTime= response.elapsedTime;
-        resStatusCode=response.statusCode;
-        resStatusMessage=response.statusMessage;
-       }
-     
-     
-  
-     const user=[ today+'',reqURL+"",resStatusCode,resStatusMessage+"",resElapsedTime];
-     const sql="INSERT INTO user (requestTime,url,statusCode,statusMessage,responseTime) VALUES (?,?,?,?,?)";
-     
-     connection.query(sql, user, function(err, results) {
-         if(err) console.log(err);
-         else {
-          console.log('Current Date&Time: ',today);
-          console.log('Request time in ms', resElapsedTime);
-          console.log('Status code: ',resStatusCode);
-          console.log('Status Message:',resStatusMessage);
-           console.log("Data added\n");
-          };
-     });
-   });
 
+      if (err) {
+        resElapsedTime = 0;
+        resStatusCode = 404;
+        resStatusMessage = "Not Found";
+      } else {
+        resElapsedTime = response.elapsedTime;
+        resStatusCode = response.statusCode;
+        resStatusMessage = response.statusMessage;
+      }
+
+      const user = [
+        today + "",
+        reqURL + "",
+        resStatusCode,
+        resStatusMessage + "",
+        resElapsedTime
+      ];
+      const sql =
+        "INSERT INTO user (requestTime,url,statusCode,statusMessage,responseTime) VALUES (?,?,?,?,?)";
+
+      connection.query(sql, user, function(err, results) {
+        if (err) console.log(err);
+        else {
+          console.log("Current Date&Time: ", today);
+          console.log("Request time in ms", resElapsedTime);
+          console.log("Status code: ", resStatusCode);
+          console.log("Status Message:", resStatusMessage);
+          console.log("Data added\n");
+        }
+      });
+    }
+  );
 }
 
-function truncateDataBase(){
+function truncateDataBase() {
   let sql = "TRUNCATE TABLE user;";
-    connection.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table Truncated");
-    });
-}
-
-
-function getDataBase(){
-  connection.query('SELECT * FROM user', (err,rows) => {
-    if(err) throw err;
-  
-    console.log(rows[0].url);
-    
-    // rows.forEach( (row) => {
-    //   console.log(`${row.url} lives in ${row.statusMessage}`);
-    // });
+  connection.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("Table Truncated");
   });
-  }
-
-module.exports={
- reqOnLinkSaveDB,
-  getDataBase,
-  truncateDataBase
 }
 
-
-// connection.end(function(err) {
-//   if (err) {
-//     return console.log("Error: " + err.message);
-//   }
-//   console.log("Connection is closed");
-// });
+module.exports = {
+  reqOnLinkSaveDB,
+  truncateDataBase
+};
